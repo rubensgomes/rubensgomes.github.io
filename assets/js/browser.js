51cffa5627d666d70448daf068a99086d991dccc
Rubens Gomes <Rubens.S.Gomes@gmail.com>
2021-05-06 13:43:43 +0100

    Releasing 0.0.1

---
+++ assets/js/browser.js
@@ -0,0 +1,1 @@
+!function(){"use strict";function t(){var t=document.createElement("script");t.src="/assets/vendor/js/update.npm.js",document.body.appendChild(t)}try{document.addEventListener("DOMContentLoaded",t,!1)}catch(e){console.log("Error in unsupported browser: "+e),window.attachEvent("onload",t)}}();
